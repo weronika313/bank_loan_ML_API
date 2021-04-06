47c2f074636200b27a9de51899a069a632aa4057
weronikazochowiec <veronica.zochowiec@gmail.com>
2021-04-06 18:13:25 +0200

    add endpoints models

---
+++ apps/endpoints/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class Endpoint(models.Model):
+    name = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+
+
+class MachineLearningAlgorithm(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=1000)
+    code = models.CharField(max_length=50000)
+    version = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    parent_endpoint = models.ForeignKey(Endpoint, on_delete=models.CASCADE)
+
+
+class MLAlgorithmStatus(models.Model):
+    status = models.CharField(max_length=128)
+    active = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    parent_algorithm = models.ForeignKey(MachineLearningAlgorithm, on_delete=models.CASCADE, related_name="status")
+
+
+class MLRequest(models.Model):
+    input_data = models.CharField(max_length=10000)
+    full_response = models.CharField(max_length=10000)
+    response = models.CharField(max_length=10000)
+    feedback = models.CharField(max_length=10000, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    parent_algorithm = models.ForeignKey(MachineLearningAlgorithm, on_delete=models.CASCADE)
+
+
