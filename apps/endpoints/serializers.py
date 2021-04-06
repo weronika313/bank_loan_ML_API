0bbc6166a1ee4819e04a88f9ba2ba368e7520e51
weronikazochowiec <veronica.zochowiec@gmail.com>
2021-04-06 18:39:31 +0200

    add DRF serializers

---
+++ apps/endpoints/serializers.py
@@ -0,0 +1,56 @@
+from rest_framework import serializers
+from apps.endpoints.models import Endpoint
+from apps.endpoints.models import MachineLearningAlgorithm
+from apps.endpoints.models import MLAlgorithmStatus
+from apps.endpoints.models import MLRequest
+
+
+class EndpointSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Endpoint
+        read_only_fields = ("id", "name", "created_at")
+        fields = read_only_fields
+
+
+class MLAlgorithmSerializer(serializers.ModelSerializer):
+    current_status = serializers.SerializerMethodField(read_only=True)
+
+    def get_current_status(self, algorithm):
+        return MLAlgorithmStatus.objects.filter(parent_algorithm=algorithm).latest('created_at').status
+
+    class Meta:
+        model = MachineLearningAlgorithm
+        read_only_fields = ("id", "name", "description", "code",
+                            "version", "created_at",
+                            "parent_endpoint", "current_status")
+        fields = read_only_fields
+
+
+class MLAlgorithmStatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MLAlgorithmStatus
+        read_only_fields = ("id", "active")
+        fields = ("id", "active", "status", "created_at",
+                  "parent_algorithm")
+
+
+class MLRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MLRequest
+        read_only_fields = (
+            "id",
+            "input_data",
+            "full_response",
+            "response",
+            "created_at",
+            "parent_algorithm",
+        )
+        fields =  (
+            "id",
+            "input_data",
+            "full_response",
+            "response",
+            "feedback",
+            "created_at",
+            "parent_algorithm",
+        )
