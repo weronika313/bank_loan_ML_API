c385a375711afde2ffc76d15df8efcfa67b881a2
weronikazochowiec <veronica.zochowiec@gmail.com>
2021-04-09 17:57:28 +0200

    add voting classifier ml code

---
+++ apps/ml/income_classifier/voting_classifier.py
@@ -0,0 +1,55 @@
+import joblib
+import pandas as pd
+
+
+class VotingClassifier:
+    def __init__(self):
+        self.model = joblib.load("research/voting_classifier.joblib")
+        self.ohe_col = joblib.load("research/all_columns.joblib")
+        self.values_fill_missing = joblib.load("research/train_mode.joblib")
+
+    def predict(self, input_data):
+        return self.model.predict_proba(input_data)
+
+    def preprocessing(self, input_data):
+        input_data = pd.DataFrame(input_data, index=[0])
+        input_data.fillna(self.values_fill_missing)
+        input_data = self.ohe_values(input_data)
+
+        return input_data
+
+    def ohe_values(self, input_data):
+        cat_columns = [
+            "Gender",
+            "Married",
+            "Education",
+            "Self_Employed",
+            "Property_Area",
+        ]
+        df_processed = pd.get_dummies(input_data, columns=cat_columns)
+        new_dict = {}
+
+        for i in self.ohe_col:
+            if i in df_processed.columns:
+                new_dict[i] = df_processed[i].values
+            else:
+                new_dict[i] = 0
+
+        new_df = pd.DataFrame(new_dict)
+        return new_df
+
+    def postprocessing(self, input_data):
+        label = "Rejected"
+        if input_data[1] > 0.5:
+            label = "Approved"
+        return {"probability": input_data[1], "label": label, "status": "OK"}
+
+    def compute_prediction(self, input_data):
+        try:
+            input_data = self.preprocessing(input_data)
+            prediction = self.predict(input_data)[0]
+            prediction = self.postprocessing(prediction)
+        except Exception as e:
+            return {"status": "Error", "message": str(e)}
+
+        return prediction
--- apps/ml/tests.py
+++ apps/ml/tests.py
@@ -6,6 +6,10 @@ from apps.ml.income_classifier.random_forest import RandomForestClassifier
 from apps.ml.income_classifier.extra_trees import ExtraTreesClassifier
 from apps.ml.income_classifier.logistic_regression import LogisticRegressionClassifier
 from apps.ml.income_classifier.naive_bayes import NaiveBayesClassifier
+from apps.ml.income_classifier.support_vector_machine import SupportVectorMachineClassifier
+from apps.ml.income_classifier.voting_classifier import VotingClassifier
+
+
 
 from apps.ml.registry import MLRegistry
 
@@ -71,6 +75,26 @@ class MLTests(TestCase):
         self.assertTrue("label" in response)
         self.assertEqual("Approved", response["label"])
 
+    def test_evc_algorithm(self):
+        input_data = {
+            "Gender": "Male",
+            "Married": "Yes",
+            "Dependents": 2,
+            "Education": "Graduate",
+            "Self_Employed": "Yes",
+            "ApplicantIncome": 5849,
+            "CoapplicantIncome": 6000,
+            "LoanAmount": 120,
+            "Loan_Amount_Term": 360,
+            "Credit_History": 1,
+            "Property_Area": "Urban",
+        }
+        my_alg = NaiveBayesClassifier()
+        response = my_alg.compute_prediction(input_data)
+        self.assertEqual("OK", response["status"])
+        self.assertTrue("label" in response)
+        self.assertEqual("Approved", response["label"])
+
     def test_nb_algorithm(self):
         input_data = {
             "Gender": "Male",
@@ -90,6 +114,8 @@ class MLTests(TestCase):
         self.assertEqual("OK", response["status"])
         self.assertTrue("label" in response)
         self.assertEqual("Approved", response["label"])
+
+
 
     def test_registry(self):
         registry = MLRegistry()
