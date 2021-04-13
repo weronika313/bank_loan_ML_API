3c8dcf7a32021c346553829a703a8efcd502e093
weronikazochowiec <veronica.zochowiec@gmail.com>
2021-04-13 17:38:34 +0200

    change SQLite to Postgres

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_api_bank_loan',
+        'USER': 'user1',
+        'PASSWORD': 'pass1',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
